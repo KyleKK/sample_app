b5d2ff1a046904db9df38074d7767843a96305ca
Kyle Kujala-Korpela <kyle.kujala.korpela@gmail.com>
2011-10-20 16:35:26 -0500

    finished first chunk of the User model

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ end
 
 group :development do
   gem 'rspec-rails'
+  gem 'annotate'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 SampleApp::Application.routes.draw do
-
- get "users/new"
+ resources :users
+ 
  match '/signup',  :to=>'users#new'
  match '/contact', :to=>'pages#contact'
  match '/about',   :to=>'pages#about'
  match '/help',    :to=>'pages#help' 
+ 
+# match '/show',    :to=>'users#show'
 # match '/',        :to=>'pages#home'
  root              :to=>'pages#home'
 
