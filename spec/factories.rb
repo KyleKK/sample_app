56c945a730616bc51dc362d7d9d3fbb3e9a9a19f
Kyle Kujala-Korpela <kyle.kujala.korpela@gmail.com>
2011-10-24 14:41:45 -0500

    done with chapter 7

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+Factory.define  :user do |user|
+ user.name	"Kyle Kujala-Korpela"
+ user.email	"kyle.kujala.korpela@gmail.com"
+ user.password 	"foobar"
+ user.password_confirmation "foobar"
+end
+
+
